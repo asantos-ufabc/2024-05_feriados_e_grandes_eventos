b7c0c61cd405620a05f5dd8306223b8b71d89960
Alan Santos <a.santos@aluno.ufabc.edu.br>
2024-05-10 17:18:14 -0300

    atualização do readm e do primeiro script

--- script/01_.R
+++ script/01_.R
@@ -1,7 +1,8 @@
 
 # Carregamento de Pacotes -------------------------------------------------
 if(!require(pacman)) {install.packages("pacman")}
-p_load(tidyverse, tidymodels, here)
+p_load(tidyverse, tidymodels, here, usethis)
 
 usethis::use_git()
 usethis::use_github()
+usethis::use_readme_rmd()
