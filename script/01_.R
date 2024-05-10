3319ba4a3a2e56de7f76c1ad78a1cb9c68ff726c
Alan Santos <a.santos@aluno.ufabc.edu.br>
2024-05-10 15:53:26 -0300

    Initial commit

---
+++ script/01_.R
@@ -0,0 +1,7 @@
+
+# Carregamento de Pacotes -------------------------------------------------
+if(!require(pacman)) {install.packages("pacman")}
+p_load(tidyverse, tidymodels, here)
+
+usethis::use_git()
+usethis::use_github()
